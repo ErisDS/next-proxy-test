d9d35c141fed207a4ef4a44a82dd3332cc2008cd
Hannah Wolfe <github.erisds@gmail.com>
2022-03-10 12:34:59 +0000

    testing subdirectory config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/journal/:path*",
+        destination: "https://next-proxy-test.ghost.io/journal/:path*",
+      }      
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/journal/:path*",
+        headers: [{ key: "x-forwarded-host", value: "next-proxy-test.vercel.app" }],
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
